1c68a3b20dab8bebf95cad170daf2dca11f35545
ghostdsb <dibyanshu.bhoi@gmail.com>
2022-10-16 02:58:10 +0530

    feat: ipfs preview

--- inspector_daya/lib/inspector_daya_web/live/page_live.ex
+++ inspector_daya/lib/inspector_daya_web/live/page_live.ex
@@ -1,6 +1,7 @@
 defmodule InspectorDayaWeb.PageLive do
   @moduledoc false
   use InspectorDayaWeb, :live_view
+  # alias InspectorDayaWeb.Router.Helpers, as: Routes
   @impl true
   def mount(_params, _session, socket) do
     {:ok,
@@ -23,22 +24,63 @@ defmodule InspectorDayaWeb.PageLive do
          },
          v0: "",
          v1: "",
-         version: 0
-       }
+         version: 0,
+        },
+        ipfs_details: %{
+          hash: "",
+          links: [],
+          size: 0,
+          type: ""
+        }
      )}
   end
 
   @impl true
-  def handle_event("decode", %{"cid-value" => cid} = _params, socket) do
-    cid_details =
-      InspectorDaya.Cid.decode(cid)
-      |> IO.inspect()
-
-    socket =
-      socket
-      |> assign(decoded_details_display: "block")
-      |> assign(cid_details: cid_details)
-
+  def handle_params(%{"cid" => cid}=_params, _uri, socket) do
+    socket = decode(cid, socket)
     {:noreply, socket}
+  end
+
+  @impl true
+  def handle_params(_params, _uri, socket) do
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("decode", %{"cid-value" => cid} = _params, socket) do
+    socket = decode(cid, socket)
+    {:noreply, push_redirect(socket, to: "/#{cid}")}
+  end
+
+  @impl true
+  def handle_event("decode", %{"value" => cid} = _params, socket) do
+    cid |> IO.inspect(label: "CID")
+    socket = decode(cid, socket)
+    {:noreply, push_redirect(socket, to: "/#{cid}")}
+  end
+
+  defp decode(cid, socket) do
+    cid_details = InspectorDaya.Cid.decode(cid)
+
+    {:ok, %{"Objects" => %{
+      ^cid => %{
+        "Hash" => hash,
+        "Links" => links,
+        "Size" => size,
+        "Type" => type
+      }
+    }}} = Ipfx.file_ls(cid)
+
+      ipfs_details = %{
+        hash: hash,
+        links: links,
+        size: size,
+        type: type
+      }
+    socket
+    |> assign(cid: cid)
+    |> assign(decoded_details_display: "block")
+    |> assign(cid_details: cid_details)
+    |> assign(ipfs_details: ipfs_details)
   end
 end
--- inspector_daya/lib/inspector_daya_web/router.ex
+++ inspector_daya/lib/inspector_daya_web/router.ex
@@ -18,6 +18,7 @@ defmodule InspectorDayaWeb.Router do
     pipe_through :browser
 
     live "/", PageLive, :index
+    live "/:cid", PageLive, :index
   end
 
   # Other scopes may use custom stacks.
