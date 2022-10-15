68202aa673ba1b0030f94c17ab7c40483caf605f
madclaws <pavanan.anandu63@gmail.com>
2022-10-15 14:35:50 +0530

    feat: rendered a basic liveview

--- inspector_daya/lib/inspector_daya_web/router.ex
+++ inspector_daya/lib/inspector_daya_web/router.ex
@@ -17,7 +17,7 @@ defmodule InspectorDayaWeb.Router do
   scope "/", InspectorDayaWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    live "/", PageLive, :index
   end
 
   # Other scopes may use custom stacks.
